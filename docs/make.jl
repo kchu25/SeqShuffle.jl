c8de86da74eac1f92f3bbc642eb5dbc82166f3e5
kchu25 <skchu@wustl.edu>
2022-05-19 23:20:24 -0500

    ok

--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,21 @@
 using SeqShuffle
 using Documenter
 
+DocMeta.setdocmeta!(SeqShuffle, :DocTestSetup, :(using SeqShuffle); recursive=true)
+
 makedocs(
+    modules = [SeqShuffle],
     sitename = "SeqShuffle",
-    format = Documenter.HTML(),
-    modules = [SeqShuffle]
+    repo="https://github.com/kchu25/SeqShuffle.jl/blob/{commit}{path}#{line}",
+    authors="Shane Kuei Hsien Chu (skchu@wustl.edu)",
+    sitename="SeqShuffle.jl",
+    format = Documenter.HTML(prettyurls=get(ENV, "CI", "false") == "true",
+                             canonical="https://kchu25.github.io/SeqShuffle.jl",
+                             assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
 )
 
 # Documenter can also automatically deploy documentation to gh-pages.
